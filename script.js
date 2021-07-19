980560756f939cb968aa4a4b7189f93a5db2690f
DualityVT <87600926+DualityVT@users.noreply.github.com>
2021-07-19 14:56:06 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var clientID = "p740vgguey37jp2ki6qdd61c2bycsv";
 var clientSecret = "c02fop8izuq0g7biwe6dgaier2qm0n";
-var redirectURL = "http://localhost";
+var redirectURL = "https://github.com/DualityVT/sandbox/blob/c3330ecfe59ef637d8793e84db1472c33cd760d3/main.html";
 
 
 const VALIDATE = "https://id.twitch.tv/oauth2/validate";
