c9300de94a82f0affbcb670f36933201d8823233
DualityVT <87600926+DualityVT@users.noreply.github.com>
2021-07-19 17:58:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,13 +8,26 @@ const AUTH = "https://id.twitch.tv/oauth2/authorize";
 const ISLIVE = "https://api.twitch.tv/kraken/streams/"
 
 function onPageLoad(){
+    if (window.location.search.length > 0){
+        let link = window.location.search;
+        link = link.slice(link.indexOf("="), link.indexOf("&"));
+        alert(link)
+    }
+}
+function onClick(){
     getToken();
 }
 function isLive(){
 /*
+url example:    
+    #access_token=rorq1oav1vcd34xnrcibfmpdl6jp4o
+    &id_token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjEifQ.eyJhdWQiOiJwNzQwdmdndWV5MzdqcDJraTZxZGQ2MWMyYnljc3YiLCJleHAiOjE2MjY3MDAxMDAsImlhdCI6MTYyNjY5OTIwMCwiaXNzIjoiaHR0cHM6Ly9pZC50d2l0Y2gudHYvb2F1dGgyIiwic3ViIjoiMTY1NTUxNjkyIiwiYXRfaGFzaCI6ImExUEFwdVRFdXZqUklHc2VOUzhWb1EiLCJhenAiOiJwNzQwdmdndWV5MzdqcDJraTZxZGQ2MWMyYnljc3YiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJjdXRlYmF0XyJ9.Hw1i9KhfXk28kSjmnv7vOIikxgZ23jUcnpAGP6DKFcommoFhZ7qYQqshsnHwZWab2T_JAsg5_QcTu3Td3SoTFHRqoIn8Agkod5eeQq7fu00RDDiJaV6ecnm583sytvAxV5peO9Oqy0ZiP2x8-EDC6qYNSCGjEkLiWiK4vdjhEpmDOg1qWxWSXUV6bhJ6sLYDYxfCcaA-vvsPYYJqNP5ls3bQM5scxy91fmqjIBhsLjCobRI2BnwO79UZpFcNLkSMSzssfuRP_vnxZmlnyx8Ik68a_Ezy5h6FaUGbaBY4oeIviCKmNOxrY4EC9vK7DIqGEDYC_1tctAdQGaebItroDg
+    &scope=openid+user_read
+    &token_type=bearer
 
+Access token are in a URL rn, catch it from there, and use it!
 window.location.search.length
-^ use this to catch token, and HOST THIS FKING SHIT ON GITHUB
+^ use this to catch token
 
 */
 }
