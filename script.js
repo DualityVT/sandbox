71b75f53288ea0982d50973294dbc9c2328b5473
DualityVT <87600926+DualityVT@users.noreply.github.com>
2021-07-19 18:10:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const AUTH = "https://id.twitch.tv/oauth2/authorize";
 const ISLIVE = "https://api.twitch.tv/kraken/streams/";
 
 function onPageLoad(){
-    alert(window.location.search.length);
+    alert(window.location.search);
     console.log("onPageLoad");
     if (window.location.search.length > 0){
         let link = window.location.search;
