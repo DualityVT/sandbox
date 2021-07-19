1ee3713bebc58efc9290c9522ee0e5c3bd341154
DualityVT <87600926+DualityVT@users.noreply.github.com>
2021-07-19 15:38:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-var clientID = "p740vgguey37jp2ki6qdd61c2bycsv";
-var clientSecret = "c02fop8izuq0g7biwe6dgaier2qm0n";
+var clientID = "";
+var clientSecret = "";
 var redirectURL = "https://github.com/DualityVT/sandbox/blob/c3330ecfe59ef637d8793e84db1472c33cd760d3/main.html";
-
-
+var token = "";
 const VALIDATE = "https://id.twitch.tv/oauth2/validate";
 const AUTH = "https://id.twitch.tv/oauth2/authorize";
 const ISLIVE = "https://api.twitch.tv/kraken/streams/"
 
 function onPageLoad(){
-    isLive();
+    getToken();
 }
 function isLive(){
 /*
@@ -20,7 +19,7 @@ window.location.search.length
 }
 function getToken(){
     let url = AUTH;
-    url += "?client_id=" + clientID;
+    url += "?client_id=" + document.getElementById(clientID).innerHTML;
     url += "&redirect_uri=" + redirectURL;
     url += "&response_type=token+id_token"
     url += "&scope=openid user_read"
